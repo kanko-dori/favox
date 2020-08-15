9441f3f1db1431ec990641c01b5bd51e73bba079
ssssota <tommysouta@gmail.com>
2020-08-15 22:51:52 +0900

    💄 Fix style

--- hosting/src/components/FavItem.tsx
+++ hosting/src/components/FavItem.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Item } from '../types/types';
 import FavItemDetail from './FavItemDetail';
+import classes from './FavItem.module.css';
 
 interface Props {
   item: Item
@@ -10,7 +11,14 @@ const FavItem: React.FC<Props> = (props: Props) => {
   const [open, setOpen] = useState(false);
   return (
     <>
-      <div title={props.item.title} role="button" onClick={() => setOpen(true)} aria-hidden="true">
+      <div
+        className={classes.container}
+        style={{ backgroundColor: props.item.color }}
+        title={props.item.title}
+        role="button"
+        onClick={() => setOpen(true)}
+        aria-hidden="true"
+      >
         <img src={props.item.imageUrl} alt={props.item.title} />
       </div>
       <FavItemDetail open={open} onClose={() => setOpen(false)} item={props.item} />
