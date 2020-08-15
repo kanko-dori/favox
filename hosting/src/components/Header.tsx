8771fa0c37ff4310185fcaf0787e452b60477475
ssssota <tommysouta@gmail.com>
2020-08-15 11:47:54 +0900

    💄 Fix headers

--- hosting/src/components/Header.tsx
+++ hosting/src/components/Header.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { theme } from '../utils/constants';
+
+const style = {
+  backgroundColor: theme.main,
+};
 
 const Header: React.FC = () => (
-  <>
-  </>
+  <header style={style}>
+    <h1>Favox</h1>
+  </header>
 );
 
 export default Header;
