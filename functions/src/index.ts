ed2ee0bc6a0234973a2ef6dbe9e4613330fb8d99
Takamichi Omori <merrytakamerry@gmail.com>
2020-08-15 10:47:37 +0900

    :sparkles: use express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,15 @@
 import * as functions from 'firebase-functions';
+import * as firebase from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import * as express from 'express';
+import * as cors from 'cors';
+
+import { ping } from './handler';
+
+const app = express();
+app.use(cors({}));
+firebase.initializeApp();
+
+app.get('/', ping);
+
+exports.api = functions.https.onRequest(app);
