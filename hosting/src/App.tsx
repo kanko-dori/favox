ec265c1c5e58122cbbe96de5837c41ef73772488
ssssota <tommysouta@gmail.com>
2020-08-15 11:48:57 +0900

    💄 Add component

--- hosting/src/App.tsx
+++ hosting/src/App.tsx
@@ -6,12 +6,16 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom';
+import Home from './pages/Home';
+import Fanvox from './pages/Fanvox';
+
+import './App.css';
 
 const App: React.FC = () => (
   <Router>
     <Switch>
-      <Route exact path="/" />
-      <Route path="/:username" />
+      <Route exact path="/" component={Home} />
+      <Route path="/:username" component={Fanvox} />
       <Redirect to="/" />
     </Switch>
   </Router>
--- hosting/src/pages/Fanvox.tsx
+++ hosting/src/pages/Fanvox.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import Header from '../components/Header';
+
 const Favox: React.FC = () => (
   <>
+    <Header />
   </>
 );
 
