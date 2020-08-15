30f4ca4ea3d73526aed66985191528a77a959057
Takamichi Omori <merrytakamerry@gmail.com>
2020-08-15 17:14:53 +0900

    :sparkles: fetch spotify playlist

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,17 +2,19 @@ import * as functions from 'firebase-functions';
 import * as firebase from 'firebase-admin';
 import * as express from 'express';
 import * as cors from 'cors';
-
-import { ping } from './handler';
+import { ping, addNewPlaylist } from './handler';
 import { verifyRequest } from './auth';
 
 firebase.initializeApp();
 
 const app = express();
 app.use(cors({}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/api/', ping);
 
 app.get('/api/secure/', verifyRequest, ping);
+app.post('/api/:userID/spotify/', addNewPlaylist);
 
 exports.api = functions.https.onRequest(app);
---
+++ functions/src/types/spotify.ts
@@ -0,0 +1,88 @@
+export interface Playlist {
+  href: string;
+  items: Item[];
+  limit: number;
+  next: null;
+  offset: number;
+  previous: null;
+  total: number;
+}
+
+export interface Item {
+  addedAt: Date;
+  addedBy: AddedBy;
+  isLocal: boolean;
+  primaryColor: null;
+  track: Track;
+  videoThumbnail: VideoThumbnail;
+}
+
+export interface AddedBy {
+  externalUrls: ExternalUrls;
+  href: string;
+  id: string;
+  type: Type;
+  uri: string;
+  name?: string;
+}
+
+export interface ExternalUrls {
+  spotify: string;
+}
+
+export enum Type {
+  Artist = 'artist',
+  User = 'user',
+}
+
+export interface Track {
+  album: Album;
+  artists: AddedBy[];
+  availableMarkets: string[];
+  discNumber: number;
+  durationMS: number;
+  episode: boolean;
+  explicit: boolean;
+  externalIds: ExternalIDS;
+  externalUrls: ExternalUrls;
+  href: string;
+  id: string;
+  isLocal: boolean;
+  name: string;
+  popularity: number;
+  previewUrl: string;
+  track: boolean;
+  trackNumber: number;
+  type: string;
+  uri: string;
+}
+
+export interface Album {
+  albumType: string;
+  artists: AddedBy[];
+  availableMarkets: string[];
+  externalUrls: ExternalUrls;
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  releaseDate: Date;
+  releaseDatePrecision: string;
+  totalTracks: number;
+  type: string;
+  uri: string;
+}
+
+export interface Image {
+  height: number;
+  url: string;
+  width: number;
+}
+
+export interface ExternalIDS {
+  isrc: string;
+}
+
+export interface VideoThumbnail {
+  url: null;
+}
