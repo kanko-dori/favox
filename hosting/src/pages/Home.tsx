35c969a1d5e4ee449d9da5d24b2cb125341465e3
ssssota <tommysouta@gmail.com>
2020-08-15 15:09:44 +0900

    ✨ Add login with twitter

--- hosting/src/pages/Home.tsx
+++ hosting/src/pages/Home.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import * as firebase from 'firebase';
+import { auth } from '../utils/firebase';
+import Button from '../components/Button';
+
+const loginWithTwitter = () => {
+  const provider = new firebase.auth.TwitterAuthProvider();
+  auth.signInWithPopup(provider).then((result) => {
+    console.log(result);
+  }).catch((error) => {
+    console.error(error);
+  });
+};
 
 const Home: React.FC = () => (
   <>
+    <Button onClick={loginWithTwitter}>login</Button>
   </>
 );
 
