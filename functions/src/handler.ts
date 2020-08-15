45e17a568665d8ce530186c6d06553fe91e025fb
Takamichi Omori <merrytakamerry@gmail.com>
2020-08-15 21:38:03 +0900

    :fire: found error

--- functions/src/handler.ts
+++ functions/src/handler.ts
@@ -61,6 +61,9 @@ export const getItems = (request: Request<getParam>, response: Response) : void 
 
 export const getItemByNumber = (request: Request<getItem>, response: Response) : void => {
   const number = parseInt(request.params.itemNumber, 10);
+  if (isNaN(number)) {
+    response.sendStatus(400);
+  }
   repository.getItems(request.params.userID).then(
     (items) => {
       const item = items[number];
