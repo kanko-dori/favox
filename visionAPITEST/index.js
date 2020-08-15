8f106d2ec5735de43614cbe355cd5130f63a974b
nori3636 <nori.k3636@gmail.com>
2020-08-15 17:08:49 +0900

    画像→背景色RGB

--- visionAPITEST/index.js
+++ visionAPITEST/index.js
@@ -17,7 +17,7 @@ async function dominantColor()  {
     let maxscore,maxFraction;
     let maxscorered,maxscoregreen, maxscoreblue;
     let downbrightR,downbrightG,downbrightB;
-    let maxFractionred,maxFractiongreen,maxFractionblue;
+    // let maxFractionred,maxFractiongreen,maxFractionblue;
     maxscore = 0;
     maxFraction = 0;
     colors.forEach(function(color){
@@ -28,21 +28,21 @@ async function dominantColor()  {
             maxscoregreen = color.color.green;
             maxscoreblue = color.color.blue
         }
-        if(color.pixelFraction >= maxFraction){ //一番使われている色のRGB値を取得
-            maxFraction = color.pixelFraction;
-            console.log("maxFraction=" + maxFraction);
-            maxFractionred = color.color.red;
-            maxFractiongreen = color.color.green;
-            maxFractionblue = color.color.blue
-        }
+        // if(color.pixelFraction >= maxFraction){ //一番使われている色のRGB値を取得
+        //     maxFraction = color.pixelFraction;
+        //     console.log("maxFraction=" + maxFraction);
+        //     maxFractionred = color.color.red;
+        //     maxFractiongreen = color.color.green;
+        //     maxFractionblue = color.color.blue
+        // }
     });
-   //補色の計算
-    let max = Math.max(maxFractionred, Math.max(maxFractiongreen, maxFractionblue));
-    let min = Math.min(maxFractionred, Math.min(maxFractiongreen, maxFractionblue));
-    let sum = max + min;
-    let compR = sum - maxFractionred;
-    let compG = sum - maxFractiongreen;
-    let compB = sum - maxFractionblue;
+//    //補色の計算
+//     let max = Math.max(maxFractionred, Math.max(maxFractiongreen, maxFractionblue));
+//     let min = Math.min(maxFractionred, Math.min(maxFractiongreen, maxFractionblue));
+//     let sum = max + min;
+//     let compR = sum - maxFractionred;
+//     let compG = sum - maxFractiongreen;
+//     let compB = sum - maxFractionblue;
 
     //明度を落とした判定
     if(Math.abs(maxscorered-maxscoregreen) < 10 && Math.abs(maxscorered - maxscoreblue) < 10 && maxscorered < 100 ){//黒なら明度を上げる
@@ -59,9 +59,9 @@ async function dominantColor()  {
 
     
 
-    if(maxFractionred ==  maxscorered && maxFractiongreen == maxscoregreen && maxFractionblue == maxscoreblue)
-        console.log("背景色に補色を提案｛R:"+　compR + ",G:"+ compG + ",B:"+ compB + "}");
-    else
+    // if(maxFractionred ==  maxscorered && maxFractiongreen == maxscoregreen && maxFractionblue == maxscoreblue && maxFraction )
+    //     console.log("背景色に補色を提案｛R:"+　compR + ",G:"+ compG + ",B:"+ compB + "}");
+    // else
         console.log("背景色に明度を落とした色を提案｛R:"+ downbrightR + ",G:"+ downbrightG + ",B:"+ downbrightB + "}");
 
 }
