2914881ded243d8d79cffc419d015be6baf81d27
ssssota <tommysouta@gmail.com>
2020-08-16 10:52:30 +0900

    💄 Add components

--- hosting/src/components/PostItem.tsx
+++ hosting/src/components/PostItem.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import CreateItemButton from './CreateItemButton';
+import EditItem from './EditItem';
 
 const PostItem: React.FC = () => {
   const [open, setOpen] = useState(false);
   return (
     <>
       <CreateItemButton onClick={() => setOpen(true)} />
+      <EditItem open={open} onClose={() => setOpen(false)} />
     </>
   );
 };
