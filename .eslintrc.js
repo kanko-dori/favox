b94c45dfea648b133f30fa151ef5ba1de58bb455
Takamichi Omori <merrytakamerry@gmail.com>
2020-11-02 23:58:42 +0900

    :sparkles: Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,34 @@
+// .eslintrc.js
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   settings: { react: { version: 'detect' } },
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
   },
   extends: [
-    'eslint:recommended', // Default rules
-    'plugin:@typescript-eslint/recommended', // TypeScript rules
-    'plugin:react/recommended', // React rules
-    'plugin:react-hooks/recommended', // React hooks rules
-    'plugin:jsx-a11y/recommended', // Accessibility rules
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier/@typescript-eslint', // Prettier plugin
+    'plugin:prettier/recommended', // Prettier recommended rules 
   ],
   rules: {
-    'react/prop-types': 'off', // We will use TypeScript's types for component props instead.
-    'react/react-in-jsx-scope': 'off', // No need to import React with Next.js
-    'jsx-a11y/anchor-is-valid': 'off', // This rule is not compatible with how Next.js's <Link />
-    // I suggest this setting for requiring return types on functions only where usefull
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
     '@typescript-eslint/explicit-function-return-type': [
-      'warn', {
+      'warn',
+      {
         allowExpressions: true,
-        allowConciseArrowFunctionExpressionsStartingWithVoid: true
-      }
+        allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+      },
     ],
+    // Includes .prettierrc.js rules
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
   },
-};
+}
