5ef3500a5ff8210cf3668af91a785c1f0f8b1e01
Sotaro Tommykawa <tommysouta@gmail.com>
2020-11-02 23:16:22 +0900

    💩 Decide dir structure

---
+++ src/backend/index.ts
@@ -0,0 +1,21 @@
+import * as functions from 'firebase-functions';
+import next from 'next';
+
+const nextjsServer = next({
+  conf: {
+    distDir: 'lib/next',
+  },
+})
+const nextjsHandle = nextjsServer.getRequestHandler()
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+export const nextApp = functions.https.onRequest((req, res) => {
+  return nextjsServer.prepare().then(() => nextjsHandle(req, res))
+})
