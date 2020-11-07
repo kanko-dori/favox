4aa39532e1d3a41e123847164f34562d5dcb4a32
Sotaro Tommykawa <tommysouta@gmail.com>
2020-11-07 16:18:40 +0900

    ✨ Add app for adding helmet

---
+++ src/frontend/pages/_app.tsx
@@ -0,0 +1,17 @@
+import Helmet from '@/components/Helmet';
+import type { AppProps } from 'next/app';
+
+const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Helmet title="Favox" description="好きを共有する">
+        <meta charSet="utf8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      </Helmet>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
