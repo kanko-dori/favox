1b91c8e70f5453e20c4182e41ebd2008f9a4bf80
Takamichi Omori <merrytakamerry@gmail.com>
2020-11-08 01:22:43 +0900

    :label: Add types

--- src/types.ts
+++ src/types.ts
@@ -28,3 +28,46 @@ export type Song = {
   color: number; // e.g. 0xFF00FF
   coverUrl: string;
 };
+
+export type spotifyTokenResponse = {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  refresh_token: string;
+  scope: string;
+};
+
+export type SpotifyUserResponse = {
+  country: string;
+  display_name: string;
+  email: string;
+  explicit_content: ExplicitContent;
+  external_urls: ExternalUrls;
+  followers: Followers;
+  href: string;
+  id: string;
+  images: Image[];
+  product: string;
+  type: string;
+  uri: string;
+};
+
+type ExplicitContent = {
+  filter_enabled: boolean;
+  filter_locked: boolean;
+};
+
+type ExternalUrls = {
+  spotify: string;
+};
+
+type Followers = {
+  href: null;
+  total: number;
+};
+
+type Image = {
+  height: null;
+  url: string;
+  width: null;
+};
