a197441a32af2c45367011413391fd7c91bf39d4
Sotaro Tommykawa <tommysouta@gmail.com>
2020-11-07 16:37:45 +0900

    🎨 Add theme color

--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app';
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <Helmet title="Favox" description="好きを共有する">
+      <Helmet title="Favox" description="好きを共有する" color="#ffcc33">
         <meta charSet="utf8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
