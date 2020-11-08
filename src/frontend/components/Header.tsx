1bcde970ed52bde51d10f27334b09e454d0fce7a
Sotaro Tommykawa <tommysouta@gmail.com>
2020-11-08 16:26:26 +0900

    💄 Add header styles

--- src/frontend/components/Header.tsx
+++ src/frontend/components/Header.tsx
@@ -1,11 +1,23 @@
+import Link from 'next/link';
+import classes from './Header.module.scss';
+
 interface Props {
   title?: string;
 }
 
-const Header: React.FC<Props> = (props: Props) => (
-  <header>
-    <h1>Favox</h1>
-    <p>{props.title}</p>
+const Header: React.FC<Props> = ({ title }) => (
+  <header className={classes.header}>
+    <Link href="/">
+      <a className={classes.favox}>
+        <h1>Favox</h1>
+      </a>
+    </Link>
+    {title && (
+      <>
+        <div className={classes.divider}></div>
+        <h3 className={classes.title}>{title}</h3>
+      </>
+    )}
   </header>
 );
 
