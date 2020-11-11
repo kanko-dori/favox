81b712052d3055ba31798019bd8741921d1816cf
Sotaro Tommykawa <tommysouta@gmail.com>
2020-11-11 15:44:18 +0900

    ✨ Fix top page

--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -1,3 +1,18 @@
-const Home: React.FC = () => <p>Hello World</p>;
+import Header from '@/components/Header';
+import Home from '@/components/Home';
+import { auth } from '@/lib/firebase';
+import { useRouter } from 'next/router';
 
-export default Home;
+const Index: React.FC = () => {
+  const router = useRouter();
+  if (auth?.currentUser?.uid) router.push('/my');
+
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+};
+
+export default Index;
