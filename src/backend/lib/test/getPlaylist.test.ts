275ae8844ce686372a719aa6b4b088dbd6d9590f
Takamichi Omori <merrytakamerry@gmail.com>
2020-11-14 22:52:41 +0900

    :sparkles: add mock

--- src/backend/lib/test/getPlaylist.test.ts
+++ src/backend/lib/test/getPlaylist.test.ts
@@ -1,12 +1,34 @@
 import { getSpotifyPlaylist } from '../getPlaylist';
 
+import SpotifyWebApi from 'spotify-web-api-node';
+// import { SinglePlaylistResponse } from 'spotify-web';
 describe('getPlaylist', () => {
   it('正しいplaylistIdからPlaylistを取得できる', async () => {
-    const data = await getSpotifyPlaylist('3hGFpoPXIJmlnd75t68UMz');
-    expect(data).toMatchSnapshot();
+    const singlePlaylistRes = {
+      body: { name: 'Test_playlist' } as SpotifyApi.SinglePlaylistResponse,
+      headers: {},
+      statusCode: 200,
+    };
+    const getPlaylistSpy = jest
+      .spyOn(SpotifyWebApi.prototype, 'getPlaylist')
+      .mockReturnValueOnce(Promise.resolve(singlePlaylistRes));
+
+    const data = await getSpotifyPlaylist('VAILD_PLAYLIST_ID');
+    expect(data.name).toBe('Test_playlist');
+    expect(getPlaylistSpy).toHaveBeenCalled();
   });
 
   it('不正なplaylistIdからPlaylistを取得できない', () => {
+    const singlePlaylistRes = {
+      body: { name: 'Test_playlist' } as SpotifyApi.SinglePlaylistResponse,
+      headers: {},
+      statusCode: 200,
+    };
+    const getPlaylistSpy = jest
+      .spyOn(SpotifyWebApi.prototype, 'getPlaylist')
+      .mockReturnValueOnce(Promise.reject(singlePlaylistRes));
+
     expect(getSpotifyPlaylist('INVALID_PLAYLIST_ID')).rejects.toThrow('Invalid playlist Id');
+    expect(getPlaylistSpy).toHaveBeenCalled();
   });
 });
