975cb895cabbf852ed2bf492fa849c49668b23ac
Takamichi Omori <merrytakamerry@gmail.com>
2020-11-24 12:55:54 +0900

    :sparkles: add new handler

--- src/backend/handler.ts
+++ src/backend/handler.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
 import { getToken, getUserInfomation } from './lib/spotify';
-import { saveUser, saveSpotifyUser } from './lib/firestore';
+import { saveUser, saveSpotifyUser, saveAlbum, savePlaylist, saveTrack } from './lib/firestore';
+
+import { fireStore } from './lib/firebase';
+import { Playlist, SpotifyIdMap, Track, User } from '../types';
+import { getPlaylist } from './lib/spotify';
+import * as functions from 'firebase-functions';
 
 export const spotifyCallbackHandler = async (req: Request, res: Response): Promise<void> => {
   const code = req.query.code as string;
@@ -18,4 +23,54 @@ export const spotifyCallbackHandler = async (req: Request, res: Response): Promi
     return;
   }
   res.redirect('/loading');
+};
+
+export const addSpotifyPlaylistHandler = async (
+  data: { playlistId: string },
+  context: functions.https.CallableContext
+): Promise<void> => {
+  const auth = context.auth;
+  if (auth == null) {
+    return;
+  }
+  const uid = auth.uid;
+  const spotifyIdMapRef = fireStore.collection('SpotifyIdMap').doc(uid);
+  const spotifyIdMapDoc = await spotifyIdMapRef.get();
+  if (!spotifyIdMapDoc.exists) {
+    return;
+  }
+
+  const spotifyIdMap = spotifyIdMapDoc.data() as SpotifyIdMap;
+  const spotifyUserRef = fireStore.collection('Users').doc(spotifyIdMap.spotifyId);
+  const spotifyUserDoc = await spotifyUserRef.get();
+  if (!spotifyUserDoc) {
+    return;
+  }
+
+  const spotifyUser = spotifyUserDoc.data() as User;
+
+  const playlistData = await getPlaylist(data.playlistId, spotifyUser.accessToken);
+  const tracks = playlistData.tracks.items.map((item) => item.track);
+  const albums = tracks.map((track) => track.album);
+
+  const saveAlbumPromises = albums.map(saveAlbum);
+  await Promise.all(saveAlbumPromises);
+  tracks.forEach((track) => {
+    track.albumRef = fireStore.doc(`Albums/${track.album.id}`);
+  });
+
+  const saveTrackPromises = tracks.map(saveTrack);
+  await Promise.all(saveTrackPromises);
+
+  playlistData.songRef = (playlistData.tracks.items.map((item) => {
+    console.log(`ref: Tracks/${item.track.id}`);
+    return fireStore.doc(`Tracks/${item.track.id}`);
+  }) as unknown) as Track[];
+
+  const playlist: Playlist = {
+    id: playlistData.id,
+    songRef: playlistData.songRef,
+  };
+
+  await savePlaylist(playlist);
 };
