bebb9c83102aec74ffc0648d6342883c9565079b
Sotaro Tommykawa <tommysouta@gmail.com>
2020-11-25 12:10:46 +0900

    ✨ user state change checking useeffect

--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -2,11 +2,14 @@ import Header from '@/components/Header';
 import Home from '@/components/Home';
 import { useUser } from '@/lib/UserProvider';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const Index: React.FC = () => {
   const router = useRouter();
   const user = useUser();
-  if (user?.uid && typeof window !== 'undefined') router.replace('/my');
+  useEffect(() => {
+    if (user?.uid && typeof window !== 'undefined') router.replace('/my');
+  }, [router, user]);
 
   return (
     <>
--- src/frontend/pages/my/index.tsx
+++ src/frontend/pages/my/index.tsx
@@ -2,11 +2,15 @@ import Header from '@/components/Header';
 import User from '@/components/User';
 import { useUser } from '@/lib/UserProvider';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const MyPage: React.FC = () => {
   const router = useRouter();
   const user = useUser();
-  if (!user?.uid && typeof window !== 'undefined') router.replace('/');
+  useEffect(() => {
+    if (!user?.uid && typeof window !== 'undefined') router.replace('/');
+  }, [router, user]);
+
   return (
     <>
       <Header />
