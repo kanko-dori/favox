fe2a561710432a3ab700f42db6399c49096c1e31
Sotaro Tommykawa <tommysouta@gmail.com>
2020-11-30 23:32:11 +0900

    💥 Remove isdev branch for url

--- src/frontend/lib/auth.ts
+++ src/frontend/lib/auth.ts
@@ -1,5 +1,4 @@
 import firebase from 'firebase/app';
-import { isDev } from './utils';
 
 export const login = (auth: firebase.auth.Auth | null): void => {
   if (!auth) throw new Error('Firebase is not initialized');
@@ -12,10 +11,7 @@ export const openSpotifyLoginPage = (uid: string): void => {
   const url = new URL('https://accounts.spotify.com/authorize');
   url.searchParams.set('client_id', '8c086f866655424db67aa2817d5ae0e9');
   url.searchParams.set('response_type', 'code');
-  url.searchParams.set(
-    'redirect_uri',
-    isDev ? 'http://localhost:5001/favoxes/us-central1/app/callback' : `${location.origin}/callback`
-  );
+  url.searchParams.set('redirect_uri', `${location.origin}/callback`);
   url.searchParams.set('state', uid);
   url.searchParams.set('scope', 'user-read-private user-read-email');
   location.href = url.href;
