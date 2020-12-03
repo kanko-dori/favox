ec53d7619327fb07ee8f9b12015a8269c0f654eb
Sotaro Tommykawa <tommysouta@gmail.com>
2020-12-04 00:06:44 +0900

    ♻ Fix some props

--- src/frontend/components/LogoutButton.tsx
+++ src/frontend/components/LogoutButton.tsx
@@ -1,19 +1,18 @@
 import { useFirebase } from '@/lib/firebase';
 import { logout } from '@/lib/auth';
+import classes from './LogoutButton.module.scss';
 
 const LogoutButton: React.FC = () => {
   const { auth } = useFirebase();
   const logoutWithFirebase = (): Promise<void> => logout(auth);
   return (
-    <button onClick={logoutWithFirebase}>
+    <button onClick={logoutWithFirebase} className={classes.button}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 100 100"
-        width="100px"
-        height="100px"
-        stroke="#cccccc"
+        stroke="#ffffff"
         fill="none"
-        strokeWidth="5px"
+        strokeWidth="8px"
       >
         <path d="M 60,20 L 25,20 L 25,80 L 60,80" />
         <path d="M 45,50 L 80,50" />
