38b24407d1d36a951ddc5a263a7f73f40c400bd5
ssssota <tommysouta@gmail.com>
2021-01-07 17:00:18 +0900

    ✨ Add numToHexColor func

--- src/frontend/lib/utils.ts
+++ src/frontend/lib/utils.ts
@@ -2,3 +2,8 @@ export const isDev =
   typeof window !== 'undefined'
     ? location.hostname === 'localhost'
     : process.env.NODE_ENV === 'development';
+
+export const numToHexColor = (num: number): string => {
+  if (!(0x000000 <= num && num <= 0xffffff)) throw new Error(`Invalid number ${num}`);
+  return '#' + num.toString(16).padStart(6, '0');
+};
