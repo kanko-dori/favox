a1acaa8d1f11d6edad9bee60d17b94f2413c7491
ssssota <tommysouta@gmail.com>
2021-01-07 17:01:18 +0900

    ✨ Add getting dominantColor effect

--- src/frontend/components/Cover.tsx
+++ src/frontend/components/Cover.tsx
@@ -1,4 +1,7 @@
-import { Track } from '../../types';
+import { useFirebase } from '@/lib/firebase';
+import { numToHexColor } from '@/lib/utils';
+import { useEffect, useState } from 'react';
+import { Album, Track } from '../../types';
 import classes from './Cover.module.scss';
 import TrackController from './TrackController';
 
@@ -7,15 +10,25 @@ interface Props {
 }
 
 const Cover: React.FC<Props> = ({ track }) => {
-  const color = track.album.images[0].dominantColor ?? 0xffffff;
+  const [color, setColor] = useState('#f5f5f5');
+  const { firestore } = useFirebase();
+
+  useEffect(() => {
+    const albumRef = firestore?.collection('Albums').doc(track.album.id);
+    return albumRef?.onSnapshot((snap) => {
+      const album = snap.data() as Album | undefined;
+      const colorNum = album?.images[0].dominantColor;
+      if (!colorNum) return;
+      try {
+        setColor(numToHexColor(colorNum));
+      } catch (e) {
+        console.error(e);
+      }
+    });
+  }, [firestore, track.album.id]);
+
   return (
-    <section
-      title={track.name}
-      className={classes.container}
-      style={{
-        backgroundColor: `#${color.toString(16)}`,
-      }}
-    >
+    <section title={track.name} className={classes.container} style={{ backgroundColor: color }}>
       <div className={`playing ${classes.progress}`} />
       <img className={classes.cover} src={track.album.images[0].url} alt="cover" />
       <TrackController />
