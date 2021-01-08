2f174a3d2f7718eb671593801f805a161981451b
Sotaro Tommykawa <tommysouta@gmail.com>
2021-01-08 12:54:44 +0900

    プレイリストを取得して表示する (#130)
    * 🏷️ Fix track.images.dominantColor nullable
    
    * 🎨 Add null check for dominantColor
    
    * ✨ Add getServerSideProps func for getting tracks
    
    * ✨ Add error check for jsx
    
    * ♻️ Refactor props for destructuring assignment
    
    * 🔒 allow getting album data
    
    * ✨ Add numToHexColor func
    
    * ✨ Add getting dominantColor effect

--- src/frontend/pages/playlist/[id].tsx
+++ src/frontend/pages/playlist/[id].tsx
@@ -1,16 +1,56 @@
 import Header from '@/components/Header';
 import TrackCovers from '@/components/TrackCovers';
-import { Track } from '../../../types';
+import { firestore } from 'firebase-admin';
+import { GetServerSideProps, GetServerSidePropsResult } from 'next';
+import { Track, Playlist } from '../../../types';
 
-const Playlist: React.FC = () => {
-  const tracks: Track[] = [];
+type Props = {
+  error?: Error;
+  tracks: Track[];
+};
 
+export const getServerSideProps: GetServerSideProps = async (
+  ctx
+): Promise<GetServerSidePropsResult<Props>> => {
+  const id = ctx.params?.id;
+  if (typeof id !== 'string') return Promise.reject(new Error('Invalid URL'));
+  return firestore()
+    .collection('Playlists')
+    .doc(id)
+    .get()
+    .then((snap) => {
+      const playlist = snap.data() as Playlist | undefined;
+      const trackRefs = playlist?.trackRefs;
+      if (!trackRefs) return Promise.reject(new Error('Playlist has no track'));
+      return Promise.all(
+        trackRefs.map((trackRef) => trackRef.get().then((snap) => snap.data() as Track | undefined))
+      );
+    })
+    .then((tracks) => ({
+      props: {
+        tracks: tracks.filter((track): track is Track => track != null),
+      },
+    }))
+    .catch((error) => {
+      console.error(error);
+      return {
+        props: { error, tracks: [] },
+      };
+    });
+};
+
+const Page: React.FC<Props> = ({ error, tracks }: Props) => {
   return (
     <>
       <Header />
+      {error && (
+        <>
+          <h3>Error!</h3>
+          <p>{error.message}</p>
+        </>
+      )}
       <TrackCovers tracks={tracks} />
     </>
   );
 };
-
-export default Playlist;
+export default Page;
