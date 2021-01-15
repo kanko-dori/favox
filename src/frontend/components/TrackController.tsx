769c251e9247da1affedbb2bbf99eeb5869b4538
Sotaro Tommykawa <tommysouta@gmail.com>
2021-01-15 17:25:41 +0900

    🚚 Play button goes to component

--- src/frontend/components/TrackController.tsx
+++ src/frontend/components/TrackController.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
+import PlayButton from './PlayButton';
 import classes from './TrackController.module.scss';
 
 type Props = {
@@ -33,22 +34,7 @@ const TrackController: React.FC<Props> = ({ audioSrc }) => {
           }}
         />
       )}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="100px"
-        height="100px"
-        viewBox="0 0 100 100"
-        onClick={() => setPlaying(!playing)}
-      >
-        {!playing ? (
-          <path d="M 13.4,0 L 100,50 L 13.4,100 z" />
-        ) : (
-          <>
-            <path d="M 10,0 l 20,0 l 0,100 l -20,0 l 0,-100 z" />
-            <path d="M 70,0 l 20,0 l 0,100 l -20,0 l 0,-100 z" />
-          </>
-        )}
-      </svg>
+      <PlayButton playing={playing} onClick={() => setPlaying(!playing)} />
       <audio className={classes.audio} ref={audioRef} src={audioSrc}>
         <track kind="captions" />
       </audio>
