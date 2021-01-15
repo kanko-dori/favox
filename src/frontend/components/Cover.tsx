7402cfc0add715443d7af14f86b233b62e9e2d59
Sotaro Tommykawa <tommysouta@gmail.com>
2021-01-15 12:58:19 +0900

    💄 Fix cover bg delay coloring

--- src/frontend/components/Cover.tsx
+++ src/frontend/components/Cover.tsx
@@ -1,6 +1,6 @@
 import { useFirebase } from '@/lib/firebase';
 import { numToHexColor } from '@/lib/utils';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Album, Track } from '../../types';
 import classes from './Cover.module.scss';
 import TrackController from './TrackController';
@@ -12,7 +12,7 @@ interface Props {
 const Cover: React.FC<Props> = ({ track }) => {
   const [color, setColor] = useState('#f5f5f5');
   const { firestore } = useFirebase();
-  const delay = useMemo(() => Math.random() * 2, []);
+  const delay = Math.random() * 2;
 
   useEffect(() => {
     const albumRef = firestore?.collection('Albums').doc(track.album.id);
@@ -29,7 +29,11 @@ const Cover: React.FC<Props> = ({ track }) => {
   }, [firestore, track.album.id]);
 
   return (
-    <section title={track.name} className={classes.container} style={{ backgroundColor: color }}>
+    <section
+      title={track.name}
+      className={classes.container}
+      style={{ backgroundColor: color, transitionDelay: `${delay}s` }}
+    >
       <img className={classes.cover} src={track.album.images[0].url} alt="cover" />
       <TrackController audioSrc={track.preview_url} />
     </section>
