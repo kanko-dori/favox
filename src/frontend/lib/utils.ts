624b1b276918b3b8f84a111fb50c602b55648b44
Sotaro Tommykawa <tommysouta@gmail.com>
2021-01-16 14:07:35 +0900

    ✨ Add gained audio node maker

--- src/frontend/lib/utils.ts
+++ src/frontend/lib/utils.ts
@@ -7,3 +7,16 @@ export const numToHexColor = (num: number): string => {
   if (!(0x000000 <= num && num <= 0xffffff)) throw new Error(`Invalid number ${num}`);
   return '#' + num.toString(16).padStart(6, '0');
 };
+
+export const createGainedAudioNode = (
+  context: AudioContext,
+  gain: number,
+  element: HTMLAudioElement
+): { sourceNode: MediaElementAudioSourceNode; gainNode: GainNode } => {
+  const sourceNode = context.createMediaElementSource(element);
+  const gainNode = context.createGain();
+  gainNode.gain.setValueAtTime(gain, context.currentTime);
+  sourceNode.connect(gainNode);
+
+  return { sourceNode, gainNode };
+};
