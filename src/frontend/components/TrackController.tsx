41bead782c048760540d76b89e7efc433b30d80a
Sotaro Tommykawa <tommysouta@gmail.com>
2021-01-16 14:08:34 +0900

    ✨ Add exclusive gained audio

--- src/frontend/components/TrackController.tsx
+++ src/frontend/components/TrackController.tsx
@@ -1,3 +1,5 @@
+import { useAudioContext } from '@/lib/AudioContextProvider';
+import { createGainedAudioNode } from '@/lib/utils';
 import { useEffect, useRef, useState } from 'react';
 import PlayButton from './PlayButton';
 import classes from './TrackController.module.scss';
@@ -8,9 +10,22 @@ type Props = {
 
 const DEFAULT_DURATION = 30;
 
+let currentPlaying: HTMLAudioElement | null = null;
+
 const TrackController: React.FC<Props> = ({ audioSrc }) => {
   const [playing, setPlaying] = useState(false);
   const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [gainNode, setGainNode] = useState<GainNode | null>(null);
+  const context = useAudioContext();
+
+  const pause = (): void => setPlaying(false);
+
+  useEffect(() => {
+    if (!context || !audioRef.current || gainNode) return;
+    const nodes = createGainedAudioNode(context, 0.1, audioRef.current);
+    nodes.gainNode.connect(context.destination);
+    setGainNode(nodes.gainNode);
+  }, [context, gainNode]);
 
   useEffect(() => {
     const audioElm = audioRef.current;
@@ -19,12 +34,20 @@ const TrackController: React.FC<Props> = ({ audioSrc }) => {
       audioElm.pause();
       return;
     }
+
+    currentPlaying?.pause();
+    currentPlaying = audioElm;
+
     audioElm.currentTime = 0;
     audioElm.play();
-    audioElm.addEventListener('ended', () => setPlaying(false));
+    audioElm.addEventListener('ended', pause);
+    audioElm.addEventListener('pause', pause);
 
-    return () => audioElm.removeEventListener('ended', () => setPlaying(false));
-  }, [playing]);
+    return () => {
+      audioElm.removeEventListener('ended', pause);
+      audioElm.removeEventListener('pause', pause);
+    };
+  }, [context, playing]);
 
   return (
     <div className={classes.container}>
@@ -37,7 +60,7 @@ const TrackController: React.FC<Props> = ({ audioSrc }) => {
         />
       )}
       <PlayButton playing={playing} onClick={() => setPlaying(!playing)} />
-      <audio className={classes.audio} ref={audioRef} src={audioSrc}>
+      <audio className={classes.audio} ref={audioRef} src={audioSrc} crossOrigin="anonymous">
         <track kind="captions" />
       </audio>
     </div>
